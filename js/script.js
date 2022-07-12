0f572b6fa2601e7835a5548e63468c9b7ebd51cc
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-07-12 17:33:15 -0400

    getRandomQuote function added

--- js/script.js
+++ js/script.js
@@ -3,31 +3,72 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// For assistance:
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-
-
+/***
+ * `quotes` array
+ ***/
+let quotes = [
+  {
+    quote: "If it doesn't challenge you, it won't change you.",
+    source: "Fred DeVito",
+  },
+  {
+    quote: "Whatever your mind can conceive and believe, it can achieve.",
+    source: "Napoleon Hill",
+    citation: "Think and Grow Rich",
+    year: 1937,
+  },
+  {
+    quote:
+      "“You are braver than you believe, stronger than you seem, and smarter than you think.",
+    source: "A.A. Milne",
+    citation: "Winnie-the-Pooh",
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    source: "Nelson Mandela",
+  },
+  {
+    quote: "The mind is everything. What you think you become.",
+    source: "Buddha",
+  },
+  {
+    quote:
+      "Strength doesn’t come from what you can do. It comes from overcoming the things you once thought you couldn’t.",
+    source: "Rikki Rogers",
+  },
+  {
+    quote:
+      "Sometimes the bad things that happen in our lives put us directly on the path to the best things that will ever happen to us.",
+    source: "Nicole Reed",
+  },
+  {
+    quote:
+      "Don’t be afraid. Be focused. Be determined. Be hopeful. Be empowered.",
+    source: "Michelle Obama",
+  },
+];
 
 /***
  * `getRandomQuote` function
-***/
-
-
+ ***/
+function getRandomQuote() {
+  let randomNumber = Math.floor(Math.random() * 7);
+  return quotes[randomNumber];
+}
 
 /***
  * `printQuote` function
-***/
-
-
+ ***/
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+//document
+//.getElementById("load-quote")
+//.addEventListener("click", printQuote, false);
